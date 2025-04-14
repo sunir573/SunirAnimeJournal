5713d322da1d4a5bd223c85b47072275aa852cb5
Sunir I <suneersabik@gmail.com>
2025-04-14 07:04:10 +0000

    The text should be in blue colour and the app theme of the app should be dark

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -8,13 +8,13 @@ const Navigation = () => {
     <nav className="bg-secondary p-4">
       <div className="container mx-auto flex justify-start space-x-4">
         <Link href="/" passHref>
-          <Button variant="link">Search</Button>
+          <Button variant="link" className="text-foreground">Search</Button>
         </Link>
         <Link href="/watched" passHref>
-          <Button variant="link">Watched</Button>
+          <Button variant="link" className="text-foreground">Watched</Button>
         </Link>
         <Link href="/watchlist" passHref>
-          <Button variant="link">Watchlist</Button>
+          <Button variant="link" className="text-foreground">Watchlist</Button>
         </Link>
       </div>
     </nav>
